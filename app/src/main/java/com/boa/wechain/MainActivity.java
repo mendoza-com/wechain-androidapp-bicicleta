293e9fbfd93c823864fb675a982e99d2d0d4a370
David Figueroa <dgfigueroa29@gmail.com>
2017-11-22 20:44:22 -0300

    Reconfigurar appId, firebase, fabric, arreglo de login with google y recupero de email desde sessión. Ocultar botones y textos innecesarios, icono oficial

--- app/src/main/java/com/boa/wechain/MainActivity.java
+++ app/src/main/java/com/boa/wechain/MainActivity.java
@@ -6,6 +6,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
@@ -28,18 +29,15 @@ import com.google.android.gms.auth.api.signin.GoogleSignIn;
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
 import com.google.android.gms.auth.api.signin.GoogleSignInClient;
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
-import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.common.SignInButton;
 import com.google.android.gms.location.ActivityRecognitionClient;
 import com.google.android.gms.location.DetectedActivity;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
-import com.google.firebase.auth.AuthCredential;
-import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
-import com.google.firebase.auth.GoogleAuthProvider;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -81,11 +79,12 @@ public class MainActivity extends AppCompatActivity implements SharedPreferences
 			mRemoveActivityUpdatesButton = findViewById(R.id.remove_activity_updates_button);
 			mStatusTextView = findViewById(R.id.status);
 			mDetailTextView = findViewById(R.id.detail);
-			findViewById(R.id.sign_in_button).setOnClickListener(this);
+			SignInButton signInButton = findViewById(R.id.sign_in_button);
+			signInButton.setSize(SignInButton.SIZE_STANDARD);
+			signInButton.setOnClickListener(this);
 			findViewById(R.id.sign_out_button).setOnClickListener(this);
 			findViewById(R.id.disconnect_button).setOnClickListener(this);
-			GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
-				.requestIdToken(Common.ID_GOOGLE).requestEmail().build();
+			GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN).requestEmail().build();
 			mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
 			mAuth = FirebaseAuth.getInstance();
 			
@@ -148,6 +147,10 @@ public class MainActivity extends AppCompatActivity implements SharedPreferences
 		super.onResume();
 		PreferenceManager.getDefaultSharedPreferences(this).registerOnSharedPreferenceChangeListener(this);
 		updateDetectedActivitiesList();
+		
+		if(mRequestActivityUpdatesButton.isEnabled()){
+			mRequestActivityUpdatesButton.performClick();
+		}
 	}
 	
 	@Override
@@ -182,7 +185,6 @@ public class MainActivity extends AppCompatActivity implements SharedPreferences
 		});
 	}
 	
-	
 	/**
 	 * Removes activity recognition updates using
 	 * {@link ActivityRecognitionClient#removeActivityUpdates(PendingIntent)}. Registers success and
@@ -193,10 +195,7 @@ public class MainActivity extends AppCompatActivity implements SharedPreferences
 		task.addOnSuccessListener(new OnSuccessListener<Void>(){
 			@Override
 			public void onSuccess(Void result){
-				Toast.makeText(mContext,
-						getString(R.string.activity_updates_removed),
-						Toast.LENGTH_SHORT)
-						.show();
+				Toast.makeText(mContext, getString(R.string.activity_updates_removed), Toast.LENGTH_SHORT).show();
 				setUpdatesRequestedState(false);
 				// Reset the display.
 				mAdapter.updateActivities(new ArrayList<DetectedActivity>());
@@ -317,14 +316,11 @@ public class MainActivity extends AppCompatActivity implements SharedPreferences
 	@Override
 	public void onStart(){
 		super.onStart();
-		// Check if user is signed in (non-null) and update UI accordingly.
 		FirebaseUser currentUser = mAuth.getCurrentUser();
 		updateUI(currentUser);
 	}
 	
 	private void signIn(){
-		//Intent signInIntent = mGoogleSignInClient.getSignInIntent();
-		//startActivityForResult(signInIntent, RC_SIGN_IN);
 		List<AuthUI.IdpConfig> providers = Arrays.asList(new AuthUI.IdpConfig.Builder(AuthUI.GOOGLE_PROVIDER).build());
 		startActivityForResult(AuthUI.getInstance().createSignInIntentBuilder().setAvailableProviders(providers).build(), RC_SIGN_IN);
 	}
@@ -332,7 +328,8 @@ public class MainActivity extends AppCompatActivity implements SharedPreferences
 	@Override
 	public void onActivityResult(int requestCode, int resultCode, Intent data){
 		super.onActivityResult(requestCode, resultCode, data);
-		if (requestCode == RC_SIGN_IN) {
+		System.out.println("requestCode: "+requestCode+" resultCode: "+resultCode+" RC_SIGN_IN: "+RC_SIGN_IN+" OK: "+ResultCodes.OK);
+		if(requestCode == RC_SIGN_IN){
 			IdpResponse response = IdpResponse.fromResultIntent(data);
 			
 			if(resultCode == ResultCodes.OK){
@@ -343,102 +340,59 @@ public class MainActivity extends AppCompatActivity implements SharedPreferences
 					System.out.println("displayname: "+user.getDisplayName());
 					System.out.println("email: "+user.getEmail());
 				}
+				
+				updateUI(user);
 			}else{
 				Log.w(TAG, "Google sign in failed");
 				updateUI(null);
 			}
 		}
-		/*
-		// Result returned from launching the Intent from GoogleSignInApi.getSignInIntent(...);
-		if(requestCode == RC_SIGN_IN){
-			Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
-			try{
-				// Google Sign In was successful, authenticate with Firebase
-				GoogleSignInAccount account = task.getResult(ApiException.class);
-				firebaseAuthWithGoogle(account);
-			}catch(ApiException e){
-				// Google Sign In failed, update UI appropriately
-				Log.w(TAG, "Google sign in failed", e);
-				// [START_EXCLUDE]
-				updateUI(null);
-				// [END_EXCLUDE]
-			}
-		}*/
-	}
-	
-	private void firebaseAuthWithGoogle(GoogleSignInAccount acct){
-		Log.d(TAG, "firebaseAuthWithGoogle:" + acct.getId());
-		// [START_EXCLUDE silent]
-		showProgressDialog();
-		// [END_EXCLUDE]
-		
-		AuthCredential credential = GoogleAuthProvider.getCredential(acct.getIdToken(), null);
-		mAuth.signInWithCredential(credential)
-				.addOnCompleteListener(this, new OnCompleteListener<AuthResult>(){
-					@Override
-					public void onComplete(@NonNull Task<AuthResult> task){
-						if(task.isSuccessful()){
-							// Sign in success, update UI with the signed-in user's information
-							Log.d(TAG, "signInWithCredential:success");
-							FirebaseUser user = mAuth.getCurrentUser();
-							updateUI(user);
-						}else{
-							// If sign in fails, display a message to the user.
-							Log.w(TAG, "signInWithCredential:failure", task.getException());
-							Toast.makeText(MainActivity.this, "Authentication failed.",
-									Toast.LENGTH_SHORT).show();
-							updateUI(null);
-						}
-						
-						// [START_EXCLUDE]
-						hideProgressDialog();
-						// [END_EXCLUDE]
-					}
-				});
 	}
 	
 	private void signOut(){
-		// Firebase sign out
 		mAuth.signOut();
-		
-		// Google sign out
 		mGoogleSignInClient.signOut().addOnCompleteListener(this,
-				new OnCompleteListener<Void>(){
-					@Override
-					public void onComplete(@NonNull Task<Void> task){
-						updateUI(null);
-					}
-				});
+			new OnCompleteListener<Void>(){
+				@Override
+				public void onComplete(@NonNull Task<Void> task){
+					updateUI(null);
+				}
+			});
 	}
 	
 	private void revokeAccess(){
-		// Firebase sign out
 		mAuth.signOut();
-		
-		// Google revoke access
 		mGoogleSignInClient.revokeAccess().addOnCompleteListener(this,
-				new OnCompleteListener<Void>(){
-					@Override
-					public void onComplete(@NonNull Task<Void> task){
-						updateUI(null);
-					}
-				});
+			new OnCompleteListener<Void>(){
+				@Override
+				public void onComplete(@NonNull Task<Void> task){
+					updateUI(null);
+				}
+			});
 	}
 	
 	private void updateUI(FirebaseUser user){
 		hideProgressDialog();
+		findViewById(R.id.sign_out_and_disconnect).setVisibility(View.GONE);
 		if(user != null){
 			mStatusTextView.setText(getString(R.string.google_status_fmt, user.getEmail()));
-			mDetailTextView.setText(getString(R.string.firebase_status_fmt, user.getUid()));
-			
 			findViewById(R.id.sign_in_button).setVisibility(View.GONE);
-			findViewById(R.id.sign_out_and_disconnect).setVisibility(View.VISIBLE);
+			
+			GoogleSignInAccount acct = GoogleSignIn.getLastSignedInAccount(this);
+			if(acct != null){
+				String personName = acct.getDisplayName();
+				String personGivenName = acct.getGivenName();
+				String personFamilyName = acct.getFamilyName();
+				String personEmail = acct.getEmail();
+				String personId = acct.getId();
+				Uri personPhoto = acct.getPhotoUrl();
+				mStatusTextView.setText(getString(R.string.google_status_fmt, personEmail));
+			}
 		}else{
 			mStatusTextView.setText("Sign Out");
 			mDetailTextView.setText(null);
 			
 			findViewById(R.id.sign_in_button).setVisibility(View.VISIBLE);
-			findViewById(R.id.sign_out_and_disconnect).setVisibility(View.GONE);
 		}
 	}
 }
